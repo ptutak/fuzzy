a50bbe0e8a1b1207733237cff0627a99c2e4f558
Piotr Tutak <p.tutak@outlook.com>
2018-11-10 17:44:09 +0100

    fuzzy

--- fuzzy.py
+++ fuzzy.py
@@ -5,7 +5,7 @@ from skfuzzy import control as ctrl
 # Nazwa i zakres wartości funkcji wejściowych i wyjściowych
 sq_error = ctrl.Antecedent(np.linspace(0, 20, 201), 'sq_error')
 speed = ctrl.Antecedent(np.linspace(5, 75, 141), 'speed')
-filter_level = ctrl.Consequent(np.linspace(1, 5, 21), 'filter_level')
+filter_level = ctrl.Consequent(np.linspace(1, 5, 21), 'filter_level', defuzzify_method='bisector')
 
 # Określenie paramtrów funkcji wyjściowych
 sq_error['low'] = fuzz.gaussmf(sq_error.universe, 0, 4)
@@ -29,21 +29,23 @@ speed.view()
 filter_level.view()
 
 # Reguły
-rule1 = ctrl.Rule(sq_error['low'] | speed['low'], filter_level['low'])
-rule2 = ctrl.Rule(speed['medium'], filter_level['medium'])
-rule3 = ctrl.Rule(speed['high'] | sq_error['medium'], filter_level['high'])
+rule1 = ctrl.Rule(sq_error['low'], filter_level['low'])
+rule2 = ctrl.Rule(sq_error['medium'] & speed['low'], filter_level['medium-high'])
+rule3 = ctrl.Rule(sq_error['high'] & speed['low'], filter_level['high'])
+rule4 = ctrl.Rule(sq_error['medium'] & speed['medium'], filter_level['medium'])
+rule5 = ctrl.Rule(sq_error['high'] & speed['medium'], filter_level['medium-high'])
+rule6 = ctrl.Rule(sq_error['medium'] & speed['high'], filter_level['medium-low'])
+rule7 = ctrl.Rule(sq_error['high'] & speed['high'], filter_level['medium'])
 
-rule1.view()
-
-filter_levelping_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
-filter_levelping = ctrl.ControlSystemSimulation(filter_levelping_ctrl)
+filter_leveling_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7])
+filter_leveling = ctrl.ControlSystemSimulation(filter_leveling_ctrl)
 
 # Obliczenie wartości funkcji wyjściowej dla zadanych paramterów wyjściowych
-filter_levelping.input['sq_error'] = 6.5
-filter_levelping.input['speed'] = 9.8
-filter_levelping.compute()
+filter_leveling.input['sq_error'] = 6.5
+filter_leveling.input['speed'] = 10
+filter_leveling.compute()
 
-print(filter_levelping.output['filter_level'])
-filter_level.view(sim=filter_levelping)
+print(filter_leveling.output['filter_level'])
+filter_level.view(sim=filter_leveling)
 
 input()
